304e563e75dc342515748a328177d08f2d9a33ff
Jared Burnham <zlolris97@gmail.com>
2019-07-02 11:11:28 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,26 @@
+def using_push(array, element)
+  array.push(element)
+end
+
+def using_unshift(array, element)
+array.unshift(element)
+end
+
+def using_pop(array)
+  array.pop()
+end
+
+def pop_with_args(array)
+  array.pop(2)
+end
+
+def using_shift(array)
+  array.shift()
+end
+
+def shift_with_args(array)
+  array.shift(2)
+end
+
+def using_concat(array1, array2)
+  array1
